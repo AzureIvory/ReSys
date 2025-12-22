0a5acfd84f2de64e575bcff534e0cc1acba86e60
AzureIvory <3062938973@qq.com>
2025-12-22 16:04:52 +0800

    2025/12/22

--- main.go
+++ main.go
@@ -1530,7 +1530,8 @@ func PE() int {
 }
 
 func main() {
-	Un7z("C:\\Users\\Administrator\\Desktop\\New folder (2)\\WePE_64_V2.3.exe", "C:\\Users\\Administrator\\Desktop\\New folder (2)\\WePE")
+	fmt.Println(BuildWIM([]string{"C:\\Users\\Administrator\\Desktop\\New folder (2)\\WePE"}, "C:\\111.wim"))
+	//Un7z("C:\\Users\\Administrator\\Desktop\\New folder (2)\\WePE_64_V2.3.exe", "C:\\Users\\Administrator\\Desktop\\New folder (2)\\WePE")
 	Uiinit()
 
 	w.Show(true)
