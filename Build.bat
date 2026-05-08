54812d54b7e0bfed2239e061ea9b5b45ee3e4c9c
AzureIvory <3062938973@qq.com>
2026-05-08 15:44:59 +0800

    2026/5/8 -1 新增图标资源

--- Build.bat
+++ Build.bat
@@ -1,3 +1,12 @@
+@echo off
+setlocal
+
 set CGO_ENABLED=0
+set GOOS=windows
 set GOARCH=386
+
+go run github.com/akavel/rsrc@v0.10.2 -arch %GOARCH% -manifest res\main.manifest -ico res\icon.ico -o rsrc_windows_%GOARCH%.syso
+if errorlevel 1 exit /b 1
+
 go build -ldflags "-H=windowsgui -s -w -extldflags=-static" -o ReSys.exe
+if errorlevel 1 exit /b 1
